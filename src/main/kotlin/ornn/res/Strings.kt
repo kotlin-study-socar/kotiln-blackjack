20c96b3acb8efccee01abc4e3514ebe5e5008e2d
SukWooJung <ggp03016@naver.com>
2022-02-24 10:28:49 +0900

    feat: resource 추가. 상수 정수, 출력을 위한 String

---
+++ src/main/kotlin/ornn/res/Strings.kt
@@ -0,0 +1,13 @@
+object Strings {
+    const val ASK_NAMES = "게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)"
+    const val DISTRIBUTE_TWO_CARDS = "에게 2장을 배부합니다."
+    const val ASK_TAKE_MORE_CARD = "는 한장의 카드를 더 받겠습니까?(예는 y, 아니오는 n)"
+    const val NOTICE_DEALER_TAKE_MORE_CARD = "딜러는 합산 16이하라서 한장의 카드를 더 받았습니다."
+    const val NOTICE_DEALER_NOT_TAKE_MORE_CARD = "딜러는 합산점수가 16 초과여서 더 이상의 카드를 더 받지않습니다.\n"
+    const val CALCULATE_SUM = " - 합산결과: "
+    const val RESULT = "\n## 최종 승패"
+
+    const val ERROR = "[ERROR] "
+    const val ERROR_NULL_INPUT_EXCEPTION = "공백을 제외한 이름을 입력해주세요"
+    const val ERROR_NOT_YES_OR_NO_EXCEPTION = "y와 n 둘중 하나를 입력해주세요"
+}
