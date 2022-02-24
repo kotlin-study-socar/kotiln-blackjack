87ab5ccb44bf3499b6139ffec7397e0020950e63
SukWooJung <ggp03016@naver.com>
2022-02-24 10:45:16 +0900

    feat: 입출력 서비스 추가

---
+++ src/main/kotlin/ornn/service/InputService.kt
@@ -0,0 +1,41 @@
+package ornn.service
+
+import ornn.exception.InputIllegalException
+
+object InputService {
+
+    fun getUsersName(): List<String> {
+        return try {
+            val input: String = readLine().toString()
+            validateNullInputException(input)
+            input.split(",")
+        } catch (e: InputIllegalException) {
+            println(e)
+            getUsersName()
+        }
+    }
+
+    fun validateNullInputException(input: String) {
+        if (input == "null" || input.contains(" ") || input.isEmpty()) {
+            throw InputIllegalException(Strings.ERROR + Strings.ERROR_NULL_INPUT_EXCEPTION)
+        }
+    }
+
+    fun isUserToTakeMoreCard(name: String): Boolean {
+        println(name + Strings.ASK_TAKE_MORE_CARD)
+        val input = readLine().toString()
+        return try {
+            validateInputIsYesOrNo(input)
+            input == "y"
+        } catch (e: InputIllegalException) {
+            println(e)
+            isUserToTakeMoreCard(name)
+        }
+    }
+
+    fun validateInputIsYesOrNo(input: String) {
+        if (input != "n" && input != "y") {
+            throw InputIllegalException(Strings.ERROR + Strings.ERROR_NOT_YES_OR_NO_EXCEPTION)
+        }
+    }
+}
