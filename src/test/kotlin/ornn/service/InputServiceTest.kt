e5287954f0a45c405ea91664dc08d1f27028546d
ornn <ornn@socar.kr>
2022-02-25 14:26:58 +0900

    refactor: 플레이어가 알파벳 Y 또는 N으로 대답하지 않았을 경우 예외테스트 추가

--- src/test/kotlin/ornn/service/InputServiceTest.kt
+++ src/test/kotlin/ornn/service/InputServiceTest.kt
@@ -6,11 +6,22 @@ import ornn.exception.InputIllegalException
 
 class InputServiceTest : BehaviorSpec({
     given("플레이어 이름을 입력 받았을 때") {
-        val input = " "
         `when`("공백이 있으면") {
+            val input = " "
             then("에러가 발생해야 한다.") {
                 assertThrows<InputIllegalException> {
                     InputService.validateNullInputException(input)
+                }
+            }
+        }
+    }
+
+    given("플레이어가 카드를 더 받을지에 대한 대답을 입력 받았을 때") {
+        `when`("y 또는 n이 아닌 다른 값을 입력했을 경우") {
+            val input = "K"
+            then("에러가 발생해야한다.") {
+                assertThrows<InputIllegalException> {
+                    InputService.validateInputIsYesOrNo(input)
                 }
             }
         }
