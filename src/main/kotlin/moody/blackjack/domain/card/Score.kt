b05a20c0e6a58a7c53ee892f5f1adc8f4a9a99b9
moody <perenoke@gmail.com>
2022-02-25 13:42:41 +0900

    feat: Score 클래스 및 test code 작성

--- src/main/kotlin/moody/blackjack/domain/card/Score.kt
+++ src/main/kotlin/moody/blackjack/domain/card/Score.kt
@@ -2,7 +2,25 @@ package moody.blackjack.domain.card
 
 class Score private constructor(val value: Int) {
 
+    fun plus(score: Score): Score {
+        return from(this.value + score.value)
+    }
+
+    fun isTwentyOne(): Boolean {
+        return value == TWENTY_ONE
+    }
+
+    fun isBust(): Boolean {
+        return value > TWENTY_ONE
+    }
+
+    fun isBiggerThen(score: Score) = this.value > score.value
+
+    fun isLessThen(score: Score) = this.value < score.value
+
     companion object {
+        private const val TWENTY_ONE = 21
+
         fun from(value: Int): Score {
             return Score(value)
         }
---
+++ src/test/kotlin/moody/blackjack/domain/card/ScoreTest.kt
@@ -0,0 +1,70 @@
+package moody.blackjack.domain.card
+
+import io.kotest.core.spec.style.AnnotationSpec
+import io.kotest.matchers.shouldBe
+
+class ScoreTest : AnnotationSpec() {
+
+    @Test
+    fun `Score끼리 plus를 하면 점수 합의 value를 가진 Score 객체가 반한된다`() {
+        //given
+        val ten = Score.from(10)
+        val seven = Score.from(7)
+
+        //when
+        val result = ten.plus(seven)
+
+        //then
+        result shouldBe Score.from(17)
+    }
+
+    @Test
+    fun `점수가 21인지 체크한다`() {
+        //given
+        val twentyOne = Score.from(21)
+
+        //when
+        val result = twentyOne.isTwentyOne()
+
+        //then
+        result shouldBe true
+    }
+
+    @Test
+    fun `점수가 21을 초과했을 경우 bust인지 체크한다`() {
+        //given
+        val bustedScore = Score.from(22)
+
+        //when
+        val result = bustedScore.isBust()
+
+        //then
+        result shouldBe true
+    }
+
+    @Test
+    fun `Score 객체를 다른 Score와 비교해서 큰지 판단한다 - 메서드 선언하는 객체 기준`() {
+        //given
+        val ten = Score.from(10)
+        val six = Score.from(6)
+
+        //when
+        val result = ten.isBiggerThen(six)
+
+        //then
+        result shouldBe true
+    }
+
+    @Test
+    fun `Score 객체를 다른 Score와 비교해서 작은 지 판단한다 - 메서드 선언하는 객체 기준`() {
+        //given
+        val ten = Score.from(10)
+        val six = Score.from(6)
+
+        //when
+        val result = six.isLessThen(ten)
+
+        //then
+        result shouldBe true
+    }
+}
