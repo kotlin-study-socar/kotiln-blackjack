9d14177e4331789d03cbb0bd4890ab6d9fdbe094
ornn <ornn@socar.kr>
2022-03-01 11:41:28 +0900

    feat: 사용자의 카드덱 합이 21이 넘었을 경우 더 이상 받을 수 없도록 하는 로직 추가

--- src/main/kotlin/ornn/res/Strings.kt
+++ src/main/kotlin/ornn/res/Strings.kt
@@ -4,6 +4,7 @@ object Strings {
     const val ASK_TAKE_MORE_CARD = "는 한장의 카드를 더 받겠습니까?(예는 y, 아니오는 n)"
     const val NOTICE_DEALER_TAKE_MORE_CARD = "딜러는 합산 16이하라서 한장의 카드를 더 받았습니다."
     const val NOTICE_DEALER_NOT_TAKE_MORE_CARD = "딜러는 합산점수가 16 초과여서 더 이상의 카드를 더 받지않습니다.\n"
+    const val NOTICE_CARDS_SUM_IS_MORE_THAN_SCORE_MAX = "플레이어의 카드합이 21이 넘었습니다. 더이상 카드를 받지 않습니다."
     const val CALCULATE_SUM = " - 합산결과: "
     const val RESULT = "\n## 최종 승패"
 
