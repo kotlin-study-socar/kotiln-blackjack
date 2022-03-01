72ed3b5bd7f3ca9a0411e6fb2d206c3aa4d84b37
ornn <ornn@socar.kr>
2022-03-01 11:41:10 +0900

    feat: 사용자의 카드덱 합이 21이 넘었을 경우 더 이상 받을 수 없도록 하는 로직 추가

--- src/main/kotlin/ornn/domain/User.kt
+++ src/main/kotlin/ornn/domain/User.kt
@@ -1,6 +1,7 @@
 package ornn.domain
 
 import ornn.dto.PlayerDto
+import ornn.res.ConstNumbers
 import ornn.service.InputService
 import ornn.service.OutputService
 
@@ -8,6 +9,11 @@ class User(name: String, cards: Cards) : Player(name, cards) {
     var result: UserResult = UserResult.BEFORE_GAME
 
     fun askedToTakeMoreCard(opCards: Cards) {
+        if (this.getCards().getScoreSum() > ConstNumbers.SCORE_MAX) {
+            OutputService.printCardsSumIsMoreThanScoreMax()
+            return
+        }
+
         if (InputService.isUserToTakeMoreCard(getName())) {
             takeCard(opCards)
             OutputService.printlnPlayer(PlayerDto.fromPlayer(this))
