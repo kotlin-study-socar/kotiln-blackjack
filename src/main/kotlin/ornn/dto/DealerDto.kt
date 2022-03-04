1f515daeebf2b7b7fe3cb67cb01a1b73987c98af
SukWooJung <ggp03016@naver.com>
2022-03-04 11:16:41 +0900

    refactor: 불필요한 필드 win, draw, lose 제거

--- src/main/kotlin/ornn/dto/DealerDto.kt
+++ src/main/kotlin/ornn/dto/DealerDto.kt
@@ -3,13 +3,10 @@ package ornn.dto
 import ornn.domain.Cards
 import ornn.domain.Dealer
 
-class DealerDto(name: String, cards: Cards, val win: Int, val lose: Int, val draw: Int) : PlayerDto(name, cards) {
+class DealerDto(name: String, cards: Cards) : PlayerDto(name, cards) {
     companion object {
         fun fromDealer(dealer: Dealer): DealerDto {
-            return DealerDto(
-                dealer.getName(), dealer.getCards(),
-                dealer.win, dealer.lose, dealer.draw
-            )
+            return DealerDto(dealer.getName(), dealer.getCards())
         }
     }
 }
