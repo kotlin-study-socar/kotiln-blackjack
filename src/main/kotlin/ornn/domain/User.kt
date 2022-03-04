b52026ea260f99929f98d26bdb386cfd742d8180
SukWooJung <ggp03016@naver.com>
2022-03-04 11:25:19 +0900

    refactor: 유저에게 카드를 더 받을건지 물어보고 받는 서비스 로직을 도메인에서 서비스 단으로 옮김

--- src/main/kotlin/ornn/domain/User.kt
+++ src/main/kotlin/ornn/domain/User.kt
@@ -1,25 +1,5 @@
 package ornn.domain
 
-import ornn.dto.PlayerDto
-import ornn.res.ConstNumbers
-import ornn.service.InputService
-import ornn.service.OutputService
-
 class User(name: String, cards: Cards) : Player(name, cards) {
     var result: UserResult = UserResult.BEFORE_GAME
-
-    fun askedToTakeMoreCard(opCards: Cards) {
-        if (this.getCards().getScoreSum() > ConstNumbers.SCORE_MAX) {
-            OutputService.printCardsSumIsMoreThanScoreMax()
-            return
-        }
-
-        if (InputService.isUserToTakeMoreCard(getName())) {
-            takeCard(opCards)
-            OutputService.printlnPlayer(PlayerDto.fromPlayer(this))
-            askedToTakeMoreCard(opCards)
-        } else {
-            OutputService.printlnPlayer(PlayerDto.fromPlayer(this))
-        }
-    }
 }
