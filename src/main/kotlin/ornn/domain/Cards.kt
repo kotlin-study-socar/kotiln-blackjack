d8406b5fdeebdc8c639c6211371efd24d4f54489
SukWooJung <ggp03016@naver.com>
2022-03-04 11:15:48 +0900

    refactor: 게임 운영에서 사용되는 카드덱 opCard -> Deck 변수명 변경. getEffectiveScore -> getScoreOfNum 메소드 명 변경.

--- src/main/kotlin/ornn/domain/Cards.kt
+++ src/main/kotlin/ornn/domain/Cards.kt
@@ -1,12 +1,10 @@
 package ornn.domain
 
-import ornn.res.ConstNumbers
+import ornn.resource.MaxNumber
 
 class Cards(private val cards: MutableList<Card>) : MutableList<Card> by cards {
     companion object {
-        private val jqkList = listOf('J', 'Q', 'K')
-
-        fun getOpCards(): Cards {
+        fun getDeck(): Cards {
             val cards = mutableListOf<Card>()
             Shape.values().forEach { makingCardOneToJQK(cards, it) }
             cards.shuffle()
@@ -17,7 +15,7 @@ class Cards(private val cards: MutableList<Card>) : MutableList<Card> by cards {
             for (num in 1..10) {
                 cards.add(Card(shape, num))
             }
-            for (c in jqkList) {
+            for (c in listOf('J', 'Q', 'K')) {
                 cards.add(Card(shape, c.code))
             }
         }
@@ -26,19 +24,25 @@ class Cards(private val cards: MutableList<Card>) : MutableList<Card> by cards {
     fun getScoreSum(): Int {
         var sum = 0
         var size = this.size
-        sortByDescending { it.getNum() }
+        sortByDescending { it.num }
         forEach {
             size--
-            sum += getEffectiveScore(it, sum, size)
+            sum += getScoreOfNum(it, sum, size)
         }
-
         return sum
     }
 
-    private fun getEffectiveScore(card: Card, sum: Int, size: Int): Int {
-        if (card.isACE() && sum + 11 <= ConstNumbers.SCORE_MAX && size == 0) {
+    private fun getScoreOfNum(card: Card, sum: Int, size: Int): Int {
+        if (isAceScoreCouldBeEleven(card, sum, size)) {
             return 11
         }
-        return card.getScoreOfNum()
+        return card.getScore()
+    }
+
+    private fun isAceScoreCouldBeEleven(card: Card, sum: Int, size: Int): Boolean {
+        if (card.isACE() && sum + 11 <= MaxNumber.SCORE_MAX && size == 0) {
+            return true
+        }
+        return false
     }
 }
