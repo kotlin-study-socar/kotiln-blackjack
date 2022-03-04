92906e19f00e1590aef6b7f88ee61fd164fae0dd
SukWooJung <ggp03016@naver.com>
2022-03-04 11:28:23 +0900

    refactor: validate로직을 Exception에서 관리 (레포지터리가 없으므로 임시로 Exeption에서 관리)

--- src/main/kotlin/ornn/exception/InputIllegalException.kt
+++ src/main/kotlin/ornn/exception/InputIllegalException.kt
@@ -1,3 +1,22 @@
 package ornn.exception
 
-class InputIllegalException(override val message: String) : IllegalArgumentException(message)
+class InputIllegalException(override val message: String) : IllegalArgumentException(message) {
+    companion object {
+        private const val ERROR = "[ERROR] "
+        private const val ERROR_NULL_INPUT_EXCEPTION = "공백을 제외한 이름을 입력해주세요"
+        private const val ERROR_NOT_YES_OR_NO_EXCEPTION = "y와 n 둘중 하나를 입력해주세요"
+
+        fun validateNullInputException(input: String) {
+            if (input == "null" || input.contains(" ") || input.isEmpty()) {
+                throw InputIllegalException(ERROR + ERROR_NULL_INPUT_EXCEPTION)
+            }
+        }
+
+        fun validateInputIsYesOrNo(input: String) {
+            val lowerCaseInput = input.lowercase()
+            if (lowerCaseInput != "n" && lowerCaseInput != "y") {
+                throw InputIllegalException(ERROR + ERROR_NOT_YES_OR_NO_EXCEPTION)
+            }
+        }
+    }
+}
